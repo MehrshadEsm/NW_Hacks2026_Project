c272c65ceb10e2e2792a01539d45ee55a6768438
MehrshadEsm <mehrshad.esm82@gmail.com>
2026-01-17 15:31:21 -0800

    Added basic text version of the game

--- src/main.js
+++ src/main.js
@@ -1,27 +1,12 @@
 import Phaser from "phaser";
+import GameScene from "./Scenes/GameScene.js";
 
 const config = {
   type: Phaser.AUTO,
   width: 1080,
   height: 720,
   backgroundColor: "#ffffff",
-  scene: {
-    preload,
-    create
-  }
+  scene: [GameScene]
 };
-
-function preload() {
-  this.load.image("enemy", "/assets/enemy.png");
-}
-
-function create() {
-  this.add.text(540, 100, "DICE DUNGEON", {
-    fontSize: "32px",
-    color: "#000000"
-  }).setOrigin(0.5);
-
-  this.add.image(540, 460, "enemy");
-}
 
 new Phaser.Game(config);
